09be036d418862fca614b0c2b56afeb9c3555afe
Lucian <lucian.cap001@gmail.com>
2019-02-24 16:01:24 -0500

    Done some work on a side project, and started some graphics hw

--- PersonalProjects/gameJS.js
+++ PersonalProjects/gameJS.js
@@ -1,9 +1,32 @@
 var graphics;
+var frameNum = 0;
 
 var canvas = document.getElementById("canvas");
 
 function getContext(){
     graphics = canvas.getContext("2d");
+    console.log("here");
 }
 
-addEventListener("mousedown", doMouseDown,)
+function draw(){
+
+    graphics.beginPath();
+    graphics.rect(200, 200, 100, 100);
+    graphics.fillStyle("red");
+    graphics.fill();
+    graphics.closePath();
+    console.log(frameNum);
+}
+
+function doFrame(){
+    frameNum++;
+
+    if(frameNum%5 == 0){
+        draw();
+    }
+}
+
+function init(){
+   requestAnimationFrame(doFrame); 
+}
+
