344910ee3d549321d0920939b2a0aa34f0de9ac2
Lucian <lucian.cap001@gmail.com>
2019-03-11 22:29:46 -0400

    Made some additions to the OS a2 hw

--- Computer Science/CS4345_OperatingSystems/a2/A2.java
+++ Computer Science/CS4345_OperatingSystems/a2/A2.java
@@ -11,11 +11,13 @@ public class A2{
         ArrayList<Integer> identifiers = Process.identifiers;
         System.out.println("Please enter your process's ID: ");
         int tempId = in.nextInt();
-        while(identifiers.contains(tempId)){
-            System.out.println("I'm sorry, the ID you entered is already being used.\nPlease enter a new ID that is listed above: ");
+        while(!identifiers.contains(tempId)){
+            System.out.println("I'm sorry, the ID you entered is already being used.\nPlease enter a new ID that isn't listed above: ");
             tempId = in.nextInt();
         }
-
+        temp.add(new Process(tempId));
+        temp.get(0).getProcessInfo();
+        temp.get(0).SJF();
     }
 
 }
@@ -25,6 +27,7 @@ class Process{
     private int id;
     private int length;
     private int priority;
+    private int waitTime = 0;
     static ArrayList<Integer> identifiers = new ArrayList<Integer>(
         Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     );
@@ -43,8 +46,7 @@ class Process{
     Process(int newId){
         Random rand = new Random();
         this.id = newId;
-        //need to fix this bit right here, user passes in id but need to remove this id from the list of available ids
-        //identifiers.remove();
+        identifiers.remove(identifiers.indexOf(newId));
         length = rand.nextInt(81) + 20;
         priority = rand.nextInt(10) + 1;
         allProcesses.add(this);
@@ -62,11 +64,42 @@ class Process{
         return this.priority;
     }
 
+    public int getWaitTime(){
+        return this.waitTime;
+    }
+
+    public void wait(int i){
+        this.waitTime += i;
+    }
+
     public void getProcessInfo(){
         System.out.print("\tProcess ID\t|\tPriority\t|\tBurst-length\t\n");
         for(int i = 0; i < allProcesses.size(); i++){
             System.out.printf("\t%5d\t\t|\t%5d\t\t|\t%5d\t\t\n", allProcesses.get(i).getId(), allProcesses.get(i).getPriority(), allProcesses.get(i).getLength());
         }
+    }
+
+
+    public void SJF(){
+        ArrayList<Process> temp = allProcesses;
+
+        temp.sort((a, b) -> a.getLength() - b.getLength());
+        
+        for(int i  = 0; i < temp.size(); i++){
+            for(int ix = i+1; ix < temp.size(); ix++){
+                temp.get(ix).wait(temp.get(i).getLength());
+            }
+        }
+        int totalWait = 0;
+        for(Process var : temp){totalWait +=var.getWaitTime();System.out.println(totalWait);}
+
+
+        System.out.print("\tProcess ID\t|\tPriority\t|\tBurst-length\t|\tScheduling algorithm\t|\tTotal waiting time\n");
+        for(int i = 0; i < allProcesses.size(); i++){
+            System.out.printf("\t%5d\t\t|\t%5d\t\t|\t%5d\t\t|\t%20s\t|\t%5d\t\n", allProcesses.get(i).getId(), allProcesses.get(i).getPriority(), allProcesses.get(i).getLength(), "Non-preemptive SJF", allProcesses.get(i).getWaitTime());
+        }
+        System.out.println("\n\nAverage wait time for non-preemptive SJF is: " + (((double)totalWait)/temp.size()));
+
     }
 
 
