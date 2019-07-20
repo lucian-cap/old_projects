e6438c683d2435ad337a4745f95d86396bb9fa01
Lucian <lucian.cap001@gmail.com>
2019-07-20 15:42:35 -0400

    Getting things even up between computers

--- PersonalProjects/udemy_cpp/ch4_1.cpp
+++ PersonalProjects/udemy_cpp/ch4_1.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
 
+int main(){
+    int favNum;
+    std::cout << "Please enter your favorite number between 1 and 100: ";
+    std::cin >> favNum;
+    std::cout << "Wow! " << favNum << " is my favorite number too!" << std::endl;
+    return 0;
+}
