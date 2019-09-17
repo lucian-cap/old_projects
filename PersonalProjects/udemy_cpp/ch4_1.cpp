34c1665dca1dd74e72033369325be623aac971eb
Lucian <lucian.cap001@gmail.com>
2019-09-17 10:51:57 -0400

    First commit on new repository

--- PersonalProjects/udemy_cpp/ch4_1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(){
-    int favNum;
-    std::cout << "Please enter your favorite number between 1 and 100: ";
-    std::cin >> favNum;
-    std::cout << "Wow! " << favNum << " is my favorite number too!" << std::endl;
-    return 0;
-}
